1b8a1ffafc5927c617b95fc047f2d713e5218a09
djaus2 <davidjones@sportronics.com.au>
2020-06-08 16:35:18 +1000

    Removeed unnecessary Bookinfo classes

--- AthsEssGymBook.Shared/BookingInfo.cs
+++ AthsEssGymBook.Shared/BookingInfo.cs
@@ -80,41 +80,5 @@ namespace AthsEssGymBook.Shared
             return duration;
         }
     }
-    /*
-        public BookingInfo()
-        {
-        }
 
-        public BookingInfo(BookingInfo_SQL bookingInfo)
-        {
-            User = bookingInfo.User;
-            Slot = bookingInfo.Slot;
-            Date = new DateTime(bookingInfo.Date);
-            Time = new TimeSpan(bookingInfo.Time * 30);
-            Duration = bookingInfo.Duration;
-        }
-
-    }
-
-    public class BookingInfo_SQL
-    {
-        public UserInfo User { get; set; }
-        public int Slot { get; set; }
-        public long Date { get; set; }
-        public int Time { get; set; }
-        public int Duration { get; set; }
-
-        public BookingInfo_SQL()
-        {
-        }
-        public BookingInfo_SQL(BookingInfo bookingInfo)
-        {
-            User = bookingInfo.User;
-            Slot = bookingInfo.Slot;
-            Date = bookingInfo.Date.Ticks;
-            Time = (int)(bookingInfo.Time.TotalMinutes / 30);
-            Duration = bookingInfo.Duration;
-        }
-
-    }*/
 }
